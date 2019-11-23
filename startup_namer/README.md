6ca33d7d94acfa4386d214a94cd4796f7fc4da9d
Ryan Gordon <ryangordon210@gmail.com>
2019-11-23 15:55:30 +0000

    Add second code lab
    You've written an interactive Flutter app that runs on both iOS and Android. In this codelab, you've:
    
    + Written Dart code.
    + Used hot reload for a faster development cycle.
    + Implemented a stateful widget, adding interactivity to your app.
    + Created a route and added logic for moving between the home route and the new route.
    + Learned about changing the look of your app's UI using themes.

--- startup_namer/README.md
+++ startup_namer/README.md
@@ -2,6 +2,16 @@
 
 A new Flutter project.
 
+This project was done using two codelabs 
++ Part 1 : https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/
++ Part 2 : https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#7
+
+Big thanks to Tommy 'Hitman' Kearns for presenting. 
+
+More info on Youtube : 
+
++ [Main channel](https://www.youtube.com/channel/UCwXdFgeE9KYzlDdR7TG9cMw/featured)
++ [Flutter Widget of the Week](https://www.youtube.com/watch?v=b_sQ9bMltGU&list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG)
 ## Getting Started
 
 This project is a starting point for a Flutter application.
@@ -14,3 +24,22 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+### Next Steps taken from the codelab 
+Learn more about the Flutter SDK:
+
++ Building layouts in Flutter tutorial
++ Add interactivity tutorial
++ Introduction to widgets
++ Flutter for Android developers
++ Flutter for React Native developers
++ Flutter for web developers
++ Flutter's Youtube channel
+Other resources include:
+
++ Build Native Mobile Apps with Flutter (a free Udacity course)
++ Flutter cookbook
++ From Java to Dart codelab
++ Bootstrap into Dart: learn more about the language
++ Please reach out to us at our mailing list. We'd love to hear from you!
--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+// This project was done using two codelabs 
+
+// Part 1 : https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/
+// Part 2 : https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#7
+
 void main() => runApp(MyApp());
 
 // #docregion MyApp
@@ -10,9 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-      brightness: Brightness.dark,
-      primaryColor: Colors.cyanAccent
-    ),
+          brightness: Brightness.dark, primaryColor: Colors.cyanAccent),
       title: 'Startup Name Generator',
       home: RandomWords(),
     );
@@ -24,8 +27,42 @@ class MyApp extends StatelessWidget {
 // #docregion RWS-var
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<
+      WordPair>(); // Add a saved set to the RandomWordsState. The set will store the word pairings that the user favourited. Set does not allow duplicates
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
   // #enddocregion RWS-var
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return Scaffold(
+            // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), // ... to here.
+    );
+  }
 
   // #docregion _buildSuggestions
   Widget _buildSuggestions() {
@@ -45,11 +82,29 @@ class RandomWordsState extends State<RandomWords> {
 
   // #docregion _buildRow
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair); // Add this line.
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      // trailing will be the icon at the end of the ListTile widget.
+      trailing: Icon(
+        //Select both the displayState and the color of the Icon using ternary operators
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      // Add a handler for when the device is tapped
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
   // #enddocregion _buildRow
@@ -60,6 +115,10 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
